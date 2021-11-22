a6211ee16014fbf67aa88f8084d3c8998f1c597f
zlqzcc <zlqzcc@gmail.com>
2021-11-21 23:42:35 -0500

    split file

--- neural/fisherCtrl.m
+++ neural/fisherCtrl.m
@@ -48,7 +48,7 @@ normFisher = sqrt(totalFisher) / normcst;
 figure(1); hold on;
 plot(log(xRange), log(normFisher), 'LineWidth', 2, 'Color', 'yellow');
 
-%% Simple Gaussian? (sum of derivatives)
+%% Simple Gaussian (Linear fisher, sum of derivatives)
 xRange = 0.5 : 0.01 : 40;
 deriMtx = zeros(nNeuron, length(xRange));
 
@@ -70,67 +70,6 @@ normFisher = sqrt(totalFisher) / normcst;
 % Fisher information
 figure(1); hold on;
 plot(log(xRange), log(normFisher), 'LineWidth', 2, 'Color', 'black');
-
-%% Noise correlation
-logPref = log(fitPara(:, 5) + 0.1);
-corrMtx = zeros(nNeuron, nNeuron);
-
-% 0.5, 1.0, 5.0
-L = 5.0;
-for i = 1:nNeuron
-    for j = 1:nNeuron
-        if i == j
-            corrMtx(i, j) = 1.0;
-        else
-            prefDist = abs(logPref(i) - logPref(j));
-            corrMtx(i, j) = exp(-prefDist / L);            
-        end
-    end
-end
-
-% A = VDV'
-% A_inv = (V_inv)' D_inv V_inv
-
-[V, D] = eig(corrMtx);
-[D, idx] = sort(diag(D), 'descend');
-
-V = V(:, idx);
-V_inv = inv(V);
-
-%% Fisher information
-% only inverting the stable part of the covariance matrix
-cutOff = sum(D > 1e-4);
-
-leftMtx = deriMtx' * V_inv';
-rigtMtx = V_inv * deriMtx;
-totalFisher = leftMtx(:, 1:cutOff) * ...
-    inv(diag(D(1:cutOff))) * ...
-    rigtMtx(1:cutOff, :);
-
-totalFisher = diag(totalFisher);
-
-normcst = trapz(xRange, sqrt(totalFisher)) * 2;
-normFisher = sqrt(totalFisher) / normcst;
-
-% Fisher information
-figure(1); hold on;
-plot(log(xRange), log(normFisher), 'LineWidth', 2);
-
-%% Corr hist
-figure(2);
-indexMtx = triu(~eye(nNeuron));
-corrMtx = corrMtx(indexMtx);
-
-hst = histogram(corrMtx(:), 30, 'Normalization', 'probability');
-hst.FaceColor = [0.25, 0.25, 0.25];
-hst.EdgeColor = [1.0, 1.0, 1.0];
-
-xlim([0, 1]);
-xticks([0 : 0.2 : 1]);
-set(gca, 'TickDir', 'out'); box off;
-
-axisObj = gca;
-axisObj.LineWidth = 0.2;
 
 %% Figure format
 labelPos = [0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0];
