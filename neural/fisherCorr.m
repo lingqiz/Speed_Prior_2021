e6f52c1b4b941f34683fb27507027e9c6d9a0b24
lingqiz <zlqzcc@gmail.com>
2021-11-22 12:32:59 -0500

    minor update to plotting

--- neural/fisherCorr.m
+++ neural/fisherCorr.m
@@ -12,7 +12,7 @@ for idx = 1 : nNeuron
     para = fitPara(idx, :);
     tuning = @(stim) tuningGauss(para(1), para(2), para(3), ...
         para(4), para(5), stim);
-    
+
     % linear Fisher information
     [fx, dfdx] = tuning(xRange);
     deriMtx(idx, :) = dfdx;
@@ -34,10 +34,9 @@ plot(log(xRange), log(normFisher), 'LineWidth', 2, 'Color', 'black');
 %% Noise correlation
 logPref = log(fitPara(:, 5) + 0.1);
 
-% L = 0.01, 0.1 has very little effect 
-for L = [0.5, 1.0, 5.0]
+for L = [0.5, 1.0, 2.5]
     corrMtx = zeros(nNeuron, nNeuron);
-    
+
     for i = 1:nNeuron
         for j = 1:nNeuron
             if i == j
@@ -48,67 +47,64 @@ for L = [0.5, 1.0, 5.0]
             end
         end
     end
-    
-    % M = VDV'
-    % M_inv = (V_inv)' D_inv V_inv
-    
+
+    % Eigen Decomposition
+    % M = VDV', M_inv = V' D_inv V
+
     [V, D] = eig(corrMtx);
     [D, idx] = sort(diag(D), 'descend');
-    
-    V = V(:, idx);
-    V_inv = inv(V);
-    
+
+    V = V(:, idx);    
+
+    % Inverse of a low-rank approximation that captures 97.5% variance
+    cumEigval = sort(unique(cumsum(D / sum(D))));
+    cutOff = floor(interp1(cumEigval, 1:length(cumEigval), 0.975));
+    corrInv = V(1:cutOff, :)' * diag(1 ./ D(1:cutOff)) * V(1:cutOff, :);
+
     %% Fisher information
-    % only inverting the stable part of the covariance matrix
-    cutOff = sum(D > 1e-1);
-    
-    leftMtx = deriMtx' * V_inv';
-    rigtMtx = V_inv * deriMtx;
-    totalFisher = leftMtx(:, 1:cutOff) * ...
-        inv(diag(D(1:cutOff))) * ...
-        rigtMtx(1:cutOff, :);
-    
-    totalFisher = diag(totalFisher);
-    
+    totalFisher = diag(deriMtx' * corrInv * deriMtx);
+
     if normalize
         normcst = trapz(xRange, sqrt(totalFisher)) * 2;
         normFisher = sqrt(totalFisher) / normcst;
     else
         normFisher = sqrt(totalFisher);
     end
-    
+
     % Fisher information
     figure(1); hold on;
     plot(log(xRange), log(normFisher), 'LineWidth', 2);
-    
+
     %% Corr hist
     figure();
     indexMtx = triu(~eye(nNeuron));
     corrMtx = corrMtx(indexMtx);
-    
-    hst = histogram(corrMtx(:), 30, 'Normalization', 'probability');
+
+    hst = histogram(corrMtx(:), 'Normalization', 'probability', 'BinEdges', 0:0.05:1);
     hst.FaceColor = [0.25, 0.25, 0.25];
     hst.EdgeColor = [1.0, 1.0, 1.0];
-    
-    xlim([0, 1]);
-    xticks([0 : 0.2 : 1]);
+
+    xlim([0, 1.0]);
+    xticks(0 : 0.2 : 1);
     set(gca, 'TickDir', 'out'); box off;
-    
+
     axisObj = gca;
     axisObj.LineWidth = 0.2;
 end
 
 %% Figure format
-figure(1);
-
-labelPos = [0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0];
-xticks(log(labelPos));
-xticklabels(arrayfun(@num2str, labelPos, 'UniformOutput', false));
-
-probPos = [0.0025, 0.005, 0.01, 0.02, 0.04, 0.08, 0.16];
-yticks(log(probPos));
-yticklabels(arrayfun(@num2str, probPos, 'UniformOutput', false));
-
-xlim(log([0.5, 40]))
-ylim(log([0.0025, 0.20]));
-set(gca, 'TickDir', 'out')
+if normalize 
+    figure(1);
+    
+    labelPos = [0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0];
+    xticks(log(labelPos));
+    xticklabels(arrayfun(@num2str, labelPos, 'UniformOutput', false));
+    
+    probPos = [0.0025, 0.005, 0.01, 0.02, 0.04, 0.08, 0.16];
+    yticks(log(probPos));
+    yticklabels(arrayfun(@num2str, probPos, 'UniformOutput', false));
+    
+    xlim(log([0.5, 40]))
+    ylim(log([0.0025, 0.20]));
+    set(gca, 'TickDir', 'out')
+end
