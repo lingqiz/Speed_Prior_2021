daf0ab709103de55f133679bba46c0b262f8d8f2
lingqiz <zlqzcc@gmail.com>
2021-11-22 16:52:41 -0500

    minor update

--- neural/fisherCorr.m
+++ neural/fisherCorr.m
@@ -34,7 +34,7 @@ plot(log(xRange), log(normFisher), 'LineWidth', 2, 'Color', 'black');
 %% Noise correlation
 logPref = log(fitPara(:, 5) + 0.1);
 
-for L = [0.5, 1.0, 2.5]
+for L = [0.5, 1.0, 2.0]
     corrMtx = zeros(nNeuron, nNeuron);
 
     for i = 1:nNeuron
@@ -42,11 +42,14 @@ for L = [0.5, 1.0, 2.5]
             if i == j
                 corrMtx(i, j) = 1.0;
             else
-                prefDist = abs(logPref(i) - logPref(j));
+                prefDist = abs(logPref(i) - logPref(j)) + 0.01;
                 corrMtx(i, j) = exp(-prefDist / L);
             end
         end
     end
+
+    % check the cond number of correlation matrix
+    disp(cond(corrMtx));
 
     % Eigen Decomposition
     % M = VDV', M_inv = V' D_inv V
@@ -54,11 +57,11 @@ for L = [0.5, 1.0, 2.5]
     [V, D] = eig(corrMtx);
     [D, idx] = sort(diag(D), 'descend');
 
-    V = V(:, idx);    
+    V = V(:, idx);
 
     % a low-rank approximation that captures 97.5% variance
     cumEigval = sort(unique(cumsum(D / sum(D))));
-    cutOff = floor(interp1(cumEigval, 1:length(cumEigval), 0.975));
+    cutOff = floor(interp1(cumEigval, 1:length(cumEigval), 0.975));    
 
     % only inverting the submatrix
     corrInv = V(1:cutOff, :)' * diag(1 ./ D(1:cutOff)) * V(1:cutOff, :);
@@ -95,17 +98,17 @@ for L = [0.5, 1.0, 2.5]
 end
 
 %% Figure format
-if normalize 
+if normalize
     figure(1);
-    
+
     labelPos = [0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0];
     xticks(log(labelPos));
     xticklabels(arrayfun(@num2str, labelPos, 'UniformOutput', false));
-    
+
     probPos = [0.0025, 0.005, 0.01, 0.02, 0.04, 0.08, 0.16];
     yticks(log(probPos));
     yticklabels(arrayfun(@num2str, probPos, 'UniformOutput', false));
-    
+
     xlim(log([0.5, 40]))
     ylim(log([0.0025, 0.20]));
     set(gca, 'TickDir', 'out')
